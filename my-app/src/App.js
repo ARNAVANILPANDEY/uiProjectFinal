f630c1a80f25632aacfecb455a3dac0e90d16da7
prahantsingh124 <prashantsingh12012@gmail.com>
2022-06-29 01:25:14 +0530

    Ui changed ( 4 Columns )

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -68,56 +68,56 @@ function App() {
    <div className="info"></div>
   
   <form>
-     
-      
     <div className="contentform">
-      
 
-
-      <div className="leftcontact">
+      <div className="firstColumn">
             <div className="form-group">
               <p>Finance Type<span>*</span></p>
               <span className="icon-case"><i className="fa fa-male"></i></span>
-                <select  style={{background: "black",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text" name="nom" class="nom" data-rule="required" data-msg="Missing data"  value={newfinance}
+                <select  style={{background: "#00807c",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text" name="nom" class="nom" data-rule="required" data-msg="Missing data"  value={newfinance}
                         onChange={(e) => setfinance(e.target.value)}>
                     <option value="Set Value">Set Value</option>
                     <option value="Loan">Loan</option>
                     <option value="Lend">Lend</option>
                 </select>
                 <div className="validation"></div>
-       </div> 
+            </div> 
 
             <div className="form-group">
             <p>Amount<span>*</span></p>
             <span className="icon-case"><i className="fa fa-user"></i></span>
-        <input type="text" name="prenom" id="prenom" data-rule="required" data-msg="Missing data" value={amount}
+            <input type="text" name="prenom" id="prenom" data-rule="required" data-msg="Missing data" value={amount}
             onChange={(e) => setAmount(e.target.value)}/>
                 <div className="validation"></div>
-      </div>
+          </div>
 
-      <div className="form-group">
-      <p>Tenure<span>*</span></p>  
-      <span className="icon-case"><i className="fa fa-envelope-o"></i></span>
-                <input type="text" name="email" id="email" data-rule="text" data-msg="Missing data." value={tenure}
-            onChange={(e) => setTenure(e.target.value)}/>
-                <div className="validation"></div>
-      </div>  
+        <div className="form-group">
+        <p>Tenure<span>*</span></p>  
+        <span className="icon-case"><i className="fa fa-envelope-o"></i></span>
+                    <input type="text" name="email" id="email" data-rule="text" data-msg="Missing data." value={tenure}
+                onChange={(e) => setTenure(e.target.value)}/>
+                    <div className="validation"></div>
+        </div>  
 
-      <div className="form-group">
-      <p>Due Date on<span>*</span></p>
-      <span className="icon-case"><i className="fa fa-male"></i></span>
-        <select style={{background: "black",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text" name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Idue}
-            onChange={(e) => setIdue(e.target.value)}>
-            <option value="Set Value">Set Value</option>
-            <option value="Anniversary">Anniversary</option>
-        </select>
-                <div className="validation"></div>
-      </div>
+        <div className="form-group">
+        <p>Due Date on<span>*</span></p>
+        <span className="icon-case"><i className="fa fa-male"></i></span>
+            <select style={{background: "#00807c",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text" name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Idue}
+                onChange={(e) => setIdue(e.target.value)}>
+                <option value="Set Value">Set Value</option>
+                <option value="Anniversary">Anniversary</option>
+            </select>
+                    <div className="validation"></div>
+        </div>
+    </div>
 
+  {/* ****************************************** */}
+
+    <div className="secondColumn">
       <div className="form-group">
       <p>Installment Computation Formula <span>*</span></p>
       <span className="icon-case"><i className="fa fa-location-arrow"></i></span>
-        <select style={{background: "black",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Installment}
+        <select style={{background: "#00807c",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Installment}
             onChange={(e) => dueInstallment(e.target.value)}>
             <option value="Set Value">Set Value</option>
             <option value="PMT">PMT</option>
@@ -127,7 +127,7 @@ function App() {
       <div className="form-group">
       <p>Advance / Arrears<span>*</span></p>
       <span className="icon-case"><i className="fa fa-location-arrow"></i></span>
-        <select style={{background: "black",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Advance}
+        <select style={{background: "#00807c",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Advance}
             onChange={(e) => setAdvance(e.target.value)}>
             <option value="Set Value">Set Value</option>
             <option value="Advance">Advance</option>
@@ -146,7 +146,7 @@ function App() {
       <div className="form-group">
       <p>Stage Based Schedule<span>*</span></p>
       <span className="icon-case"><i className="fa fa-map-marker"></i></span>
-        <select style={{background: "black",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Stagee}
+        <select style={{background: "#00807c",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Stagee}
             onChange={(e) => setStage(e.target.value)}>
             <option value="Set Value">Set Value</option>
             <option value="Yes">Yes</option>
@@ -154,17 +154,17 @@ function App() {
         </select>
                 <div className="validation"></div>
       </div>  
-
-
-
   </div>
 
-  <div className="rightcontact">  
+
+  {/* *********************************************** */}
+
+   <div className="thirdColumn">  
 
       <div className="form-group">
       <p>Day Count Convention<span>*</span></p>
       <span className="icon-case"><i className="fa fa-building-o"></i></span>
-        <select style={{background: "black",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Daycount}
+        <select style={{background: "#00807c",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}} type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={Daycount}
             onChange={(e) => setDaycount(e.target.value)} >
             <option value="Set Value">Set Value</option>
             <option value="Actual/Actual">Actual/Actual</option>
@@ -183,7 +183,7 @@ function App() {
       <div className="form-group">
       <p>First Repayment after Disbursement<span>*</span></p>
       <span className="icon-case"><i className="fa fa-info"></i></span>
-                <select style={{background: "black",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}}  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={IStage}
+                <select style={{background: "#00807c",color: "white",size:"100%",marginLeft:"2px",textAlign: "center"}}  name="nom" class="nom" data-rule="required" data-msg="Missing data" value={IStage}
             onChange={(e) => dueSItage(e.target.value)}>
                     <option value="Set Value">Set Value</option>
                     <option value="Anniversary">Anniversary</option>
@@ -194,14 +194,20 @@ function App() {
       <div className="form-group">
       <p>	Installment Frequency<span>*</span></p> 
       <span className="icon-case"><i className="fa fa-comment-o"></i></span>
-                <select  style={{background: "black",color: "white",height:"100% !important",marginLeft:"2px",textAlign: "center"}}type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data." value={InstallmentF}
+                <select  style={{background: "#00807c",color: "white",height:"100% !important",marginLeft:"2px",textAlign: "center"}}type="text"  name="nom" class="nom" data-rule="required" data-msg="Missing data." value={InstallmentF}
             onChange={(e) => setInstallmentF(e.target.value)}>
                     <option value="Set Value">Set Value</option>
                     <option value="Monthly">Monthly</option>
                 </select>
                 <div className="validation"></div>
       </div>
-      <div className="form-group">
+  </div>
+
+
+  {/* ******************************* */}
+
+  <div className="fourthColumn"> 
+    <div className="form-group">
       <p>	Disbursement Date<span>*</span></p> 
       <span className="icon-case"><i className="fa fa-comment-o"></i></span>
                 <input type="date" name="sujet" id="sujet" data-rule="required" data-msg="Missing data."value={OpenDate}
@@ -213,7 +219,7 @@ function App() {
       <span className="icon-case"><i className="fa fa-comment-o"></i></span>
                 <input type="text" name="sujet" id="sujet" data-rule="required" data-msg="Missing data." value={Damount}
             onChange={(e) => setDamount(e.target.value)}/>
-                <div className="validation"></div>Damount
+                <div className="validation"></div>
       </div>
       <div className="form-group">
       <p>	Disbursement Segment Number<span>*</span></p> 
@@ -229,14 +235,15 @@ function App() {
                 <input type="text" name="sujet" id="sujet" data-rule="required" data-msg="Missing data." value={rate}
             onChange={(e) => setrate(e.target.value)}/>
                 <div className="validation"></div>
-      </div>  
-  </div>
-  </div>
-<button onClick={hel} type="submit" className="bouton-contact" >Send</button>
-  
-</form> 
+       </div>  
     </div>
+  </div>
+  {/* ************************ */}
 
+    <button onClick={hel} type="submit" className="bouton-contact" >Send</button>
+  
+   </form> 
+    </div>
           : <Trial />
   );
 }
